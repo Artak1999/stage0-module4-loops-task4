d28936406f0b96110e9996fe71b5fa74e3067fa9
Artak1999 <dashyan.1999@mail.ru>
2022-07-07 13:43:55 +0500

    Simple examples.

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-
+        for (int i = 0; i < numberToGoUntil; i++) {
+            System.out.println(i);
+            if(i < toBreakWith) {
+                System.out.println("iterating till the end");
+                break;
+            }
+        }
     }
 }
