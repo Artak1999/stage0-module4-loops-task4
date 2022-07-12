e49aa9db3d836719b03ac3393dccf8f52dcb894c
Artak1999 <dashyan.1999@mail.ru>
2022-07-12 11:23:13 +0500

    Simple examples.

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-        for (int i = 0; i < numberToGoUntil; i++) {
+        for (int i = 1; i <= numberToGoUntil; i++) {
             System.out.println(i);
             if(numberToGoUntil < toBreakWith) {
                 System.out.println("iterating till the end");
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -4,7 +4,7 @@ public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
         int sum = 0;
         int index = 0;
-        for (int i = -2; i < lastInRow; i++) {
+        for (int i = 1; i <= lastInRow; i++) {
             if(numberToSkip > lastInRow) {
                 System.out.println("number to skip is bugger then the last");
                 break;
@@ -13,8 +13,10 @@ public class TwoRangesSum {
                 System.out.println("last number in row is negative");
                 break;
             }
-            else if(numberToSkip == i)
+            else if(numberToSkip == i) {
                 index = i;
+                continue;
+            }
             sum += i;
         }
         System.out.println("skipped sum is " + index + " counted sum is number " + sum);
